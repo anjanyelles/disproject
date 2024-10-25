efc5597f4715a464a177238cd331b8d91f01be67
anjan kumar <anjanyelle@gmail.com>
2024-10-25 19:10:56 +0530

    ch

--- src/components/presentation/header.tsx
+++ src/components/presentation/header.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import {
-  Button,
-  Typography,
-} from "@material-tailwind/react";
+import { Button, Typography } from "@material-tailwind/react";
 import ThemeProvider from "../theme-provider";
 import Navbar from "../navbar";
 
-import {
-  ArrowSmallRightIcon,
-} from "@heroicons/react/24/outline";
+import { ArrowSmallRightIcon } from "@heroicons/react/24/outline";
 
 export function HeroPresentation() {
- 
   return (
     <ThemeProvider>
       <Navbar />
-      <header className="h-full w-screen place-items-center bg-white relative px-8 py-8 lg:mb-36">
-        <div className="container mx-auto grid items-center lg:grid-cols-2">
-          <div className="text-center lg:text-left pt-32">
-            <div className="mb-8 inline-flex items-center">
+      <header className="relative w-screen h-full px-8 py-8 bg-white place-items-center lg:mb-36">
+        <div className="container grid items-center mx-auto lg:grid-cols-2">
+          <div className="pt-32 text-center lg:text-left">
+            <div className="inline-flex items-center mb-8">
               <Typography
                 variant="small"
                 className="mr-3 py-0.5 px-3 font-bold text-dark uppercase border-r border-dark"
@@ -30,30 +24,35 @@ export function HeroPresentation() {
                 variant="small"
                 className="flex items-center font-bold uppercase"
               >
-             Disaha
+                Disaha
               </Typography>
             </div>
             <Typography
               variant="h1"
               color="blue-gray"
-              className="mb-8 lg:mr-32 leading-tight font-black"
+              className="mb-8 font-black leading-tight lg:mr-32"
             >
               Helping You Reach <span className="text-blue">Your Dreams</span>
             </Typography>
             <Typography variant="lead" color="blue-gray" className="lg:pr-32">
-            Get expert advice and personalized study plans to achieve your educational goals.
+              Get expert advice and personalized study plans to achieve your
+              educational goals.
             </Typography>
-            <div className="mt-12 flex flex-wrap justify-center gap-3 lg:justify-start">
+            <div className="flex flex-wrap justify-center gap-3 mt-12 lg:justify-start">
               <a href="#pricing">
                 <Button color="dark" className="flex items-center">
-  Register Now
+                  Register Now
                 </Button>
               </a>
             </div>
           </div>
         </div>
         <div className="hidden lg:flex">
-          <img src="img11.jpg" alt="components" className="absolute -top-10 right-0 w-1/2" />
+          <img
+            src="12.png"
+            alt="components"
+            className="absolute right-0 w-1/2 -top-10"
+          />
         </div>
       </header>
 

6c1e9911374f7d059345126ac6c3ddbd03ef6f23
anjanyelle <anjanyelle@gmail.com>
2024-10-27 00:34:52 +0530

    ch

--- about1.jsx
+++ about1.jsx
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>About |</title>
+    <title>About | Play Tailwind</title>
     <link
       rel="shortcut icon"
       href="assets/images/favicon.png"
